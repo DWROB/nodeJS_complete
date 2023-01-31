cabac3d45a2904981ef7d226b49072cb844fd8e4
Dal Robinson <dalwrobinson@protonmail.com>
2023-01-31 16:40:09 +0000

    pug completed. init of handlebars

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'pug'); // pug installs itself as opt for express.Not the case for all templ. engines.
-// view engine - tells app what dynamic templ. engine using
+app.set('view engine', 'pug');
+
 app.set('views', 'views')
-// views - tells expr where to find these dynamic views.
-// default value is views so above isn't needed.  If however the html etc is in
-// a folder called templates.
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,13 +15,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // static method enter the path of folder to grant access to
 app.use(express.static(path.join(__dirname, "public")));
 
-// outsourced routes - admin filtered
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 // catch-all route
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -9,14 +9,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  // console.log(adminData.products); // rarely used as this data is available for all users.
-  // res.sendFile(path.join(rootDir, "views", "shop.html"));
-
-  // we want to use the products.
   const products = adminData.products;
-
-  // we want to render a template
-  res.render('shop', { prods: products, docTitle: 'Shop' });
+  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
