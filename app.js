f80940dc3b17e47ae2055c744aad1646c7f48d61
Dal Robinson <dalwrobinson@protonmail.com>
2023-02-01 18:29:01 +0000

    handelbars update

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path");
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.set('views', 'views')
+const rootDir = require("./util/path");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // static method enter the path of folder to grant access to
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
@@ -21,7 +23,7 @@ app.use(shopRoutes);
 // catch-all route
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -4,13 +4,20 @@ const { Router } = require("express");
 const express = require("express");
 
 const rootDir = require("../util/path");
-const adminData = require('./admin');
+const adminData = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
 });
 
 module.exports = router;
