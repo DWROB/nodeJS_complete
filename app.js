cc372eeef2711b53728130515323407693fc1bd1
Dal Robinson <dalwrobinson@protonmail.com>
2023-02-01 19:02:45 +0000

    ejs set up.  Cleaned up and removed traces of other template engines

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ const rootDir = require("./util/path");
 
 const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+app.use(express.static(path.join(rootDir, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// static method enter the path of folder to grant access to
-app.use(express.static(path.join(rootDir, "public")));
 
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
@@ -23,7 +22,7 @@ app.use(shopRoutes);
 // catch-all route
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page Not Found" });
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: 0 });
 });
 
 app.listen(3000);
