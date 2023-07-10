7a59bf5cc3990fe832b917de8f83b99987e9014b
dal.robinson <dal.robinson@itgest.pt>
2023-07-10 16:43:20 +0100

    remove assignments

--- nodejs_assignments/03_assignment/util/path.js
+++
@@ -1,4 +0,0 @@
-const path = require('path');
-// this essentially defines the path of the file that
-// the app was spun on.  the root directory.
-module.exports = path.dirname(require.main.filename);
