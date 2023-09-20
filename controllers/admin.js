1f1691bc9850db1cdf4a3003297c5b12904fcfef
dal.robinson <dal.robinson@itgest.pt>
2023-09-20 10:36:58 +0100

    125. edit page prepopulated

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
     res.render('../views/admin/edit-product', {
         pageTitle: 'Add Product',
-        path: '/admin/add-product'
+        path: '/admin/add-product',
+        editing: false
     });
 };
 
@@ -20,12 +21,22 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getEditProduct = (req, res, next) => {
-    
-    res.render('../views/admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: true
+
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        if (!product) {
+            console.log('Product not found')
+            return res.redirect('/');
+        }
+        res.render('../views/admin/edit-product', {
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: true,
+            product: product
+        });
+
     });
+    
 };
 
 exports.getProducts = (req, res, next) => {
