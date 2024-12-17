8e08932d10649ad39f442b0c6e2bf1211cf9dbf6
Darren Robinson <dwr@unknown0aafd0287b08.lan>
2024-12-17 11:12:36 +0000

    User - basic init OK

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const sequelize = require('./util/database')
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,13 +19,41 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
-    app.listen(3000);
-}).catch(error => {
-    console.log(err);
-});
+// delete products when user is deleted.
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE' });
+// User.hasMany(Product);
+
+// force true arg only to overwrite existing tables.
+sequelize
+    // .sync({ force: true })
+    .sync()
+    .then(result => {
+        return User.findByPk(1);
+    })
+    .then(user => {
+        if (!user) {
+            return User.create({name: 'Dal', email: 'pop@pop.com' });
+        }
+        return Promise.resolve(user);
+    })
+    .then(user => {
+        // console.log(user);
+        app.listen(3000);
+    })
+    .catch(error => {
+        console.log(err);
+    });
