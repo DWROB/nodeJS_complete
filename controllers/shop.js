022e961e0846cab653437d0e3145f7ef1eb23da9
Darren Robinson <dwr@unknownd6785356eca8.lan>
2024-12-29 21:26:47 +0000

    orders create OK

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require("../models/product");
-const Cart = require("../models/cart");
+const Order = require("../models/order");
 
 exports.getProducts = (req, res, next) => {
     Product.findAll()
@@ -118,6 +118,29 @@ exports.getOrders = (req, res, next) => {
     path: "/orders",
     pageTitle: "Your Orders",
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user.getCart()
+    .then(cart => {
+        return cart.getProducts();
+    })
+    .then(products => {
+        // try not to nest promises unnecessarily
+        return req.user
+            .createOrder()
+            .then(order => {
+                return order.addProducts(products.map(product => {
+                    product.orderItem = { quantity: product.cartItem.quantity };
+                    return product;
+                }))
+            })
+            .catch(err => console.log(err));
+    })
+    .then(result => {
+        res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getCheckout = (req, res, next) => {
