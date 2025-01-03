b2c266a973a994377906f9490319fa3fa45469eb
DWROB <89933924+DWROB@users.noreply.github.com>
2025-01-03 16:38:49 +0000

    adding to cart OK

--- controllers/shop.js
+++ controllers/shop.js
@@ -44,3 +44,16 @@ exports.getIndex = (req, res, next) => {
             console.log(err);
         });
 };
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId).then(product => {
+        return req.user.addToCart(product);
+    })
+    .then(result => {
+       console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+};
