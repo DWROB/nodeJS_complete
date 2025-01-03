05e01b9955278838969b154e14b8b75bcb22233a
DWROB <89933924+DWROB@users.noreply.github.com>
2025-01-03 15:44:17 +0000

    Initial user OK

--- models/user.js
+++ models/user.js
@@ -1,23 +1,46 @@
-//  user model with id, name and email - sequelize model
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+class User {
+    constructor(username, email, id) {
+        this.username = username;
+        this.email = email;
+        this._id = id ? new mongodb.ObjectId(id) : null;
     }
-});
+
+    save() {
+        const db = getDb();
+        let dbOp;
+
+        if (this._id) {
+            dbOp = db.collection('users')
+                .updateOne({ _id: this._id}, { $set: this });
+        } else {
+            dbOp = db.collection('users').insertOne(this);
+        }
+
+        return dbOp
+            .then(result => {
+                console.log(result);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+
+        // findOne removes the need for calling next() to access the first row of the cursor object.
+        return db.collection('users')
+            .findOne({ _id: new mongodb.ObjectId(userId) })
+            .then(user => {
+                console.log(user);
+                return user;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+}
 
 module.exports = User;
