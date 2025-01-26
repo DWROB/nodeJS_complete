44402127b345502bc2a5b587fb2c225c9b589446
DWROB <89933924+DWROB@users.noreply.github.com>
2025-01-26 17:01:44 +0000

    240 sessions

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,16 @@
 
 
 exports.getLogin = (req, res, next) => {
+    // NOTE: the bool in cookie is sent at string, so 'false' as string is deemed true.
+    const isLoggedIn = req.get('Cookie').split(';')[1].split('=')[1] === 'true';
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login Page'
+        pageTitle: 'Login Page',
+        isAuthenticated: req.isLoggedIn
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true')
+    res.redirect('/');
 };
